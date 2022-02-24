bb86d62a60c83f597cf1d2e90bcab607a14ea148
William Armstrong <71415525+wxarmstrong@users.noreply.github.com>
2022-02-23 19:21:00 -0800

    Add files via upload

---
+++ detect.py
@@ -0,0 +1,15 @@
+from langdetect import detect
+from langdetect import detect_langs
+
+# Checks against the most likely language detected
+def is_lang(lang, str):
+	return detect(str) == lang
+
+# Checks if the string matches the target language by at least the given probability    
+def is_lang_prob(targetLang, prb, str):
+    langs = detect_langs(str)
+    for l in langs:
+        if l.lang == targetLang and l.prob >= prb:
+            return True
+    return False
+
