ff6b0a64e617678f131d8e2382334342842005b1
William Armstrong <71415525+wxarmstrong@users.noreply.github.com>
2022-03-25 18:35:09 -0700

    Add files via upload

---
+++ inv_idx.py
@@ -0,0 +1,33 @@
+import os
+import glob
+from bs4 import BeautifulSoup
+
+def extract_words(text, word_list):
+    words = text.lower().split()
+
+    for word in words:
+        word_list.append(word)
+
+def inv_idx(dirname):
+    files = glob.glob(dirname + '/*.html')
+    idx = {}
+    for fi in files:
+        #print(fi)
+        with open(fi, 'rb') as f:
+            words = []
+            content = f.read()
+            content = content.decode('utf-16')
+            content = content.encode('utf-8')
+            #print(content)
+            soup = BeautifulSoup(content, 'html.parser')
+            for script in soup(["script", "style"]):
+                script.extract()
+            text = soup.get_text()
+            print(text)
+            extract_words(text, words)
+            for word in words:
+                if word not in idx:
+                    idx[word] = []
+                if word not in idx[word]:
+                    idx[word].append(fi)
+    return idx
